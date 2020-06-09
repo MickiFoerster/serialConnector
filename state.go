6b61d24fb2bc052986b7d7e7819ea8aa21542f13
micki <github@com-science.de>
2020-06-10 00:34:18 +0200

    state started

--- state.go
+++ state.go
@@ -12,15 +12,28 @@ type State struct {
 type Condition []func() bool
 
 type transition struct {
-	from       State
-	to         State
+	from       *State
+	to         *State
 	conditions []Condition
 }
 
 var (
-	start        State
+	start        State {
+        name: "undefined",
+    }
 	currentstate *State = &start
 	loggedoff           = State{
 		name: "loggedoff",
 	}
+	loggedin           = State{
+		name: "loggedin",
+	}
+
+    t1 = transition {
+        from: &start,
+        to: &loggedoff,
+        conditions: []Condition {
+            Condition{},
+        },
+    }
 )
