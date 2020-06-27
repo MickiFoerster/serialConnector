d082930d7c3606e077109b4666b2d971f7d8142a
Michael Foerster <Michael.Foerster@com-science.de>
2020-06-27 23:49:26 +0200

    state machine changes

--- state.go
+++ state.go
@@ -3,259 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
-	"regexp"
-	"strings"
-	"sync"
 	"time"
 )
 
 type State struct {
 	name         string
 	entranceTime time.Time
-	sent         []byte
-	sentMtx      sync.Mutex
-	received     []byte
-	receivedMtx  sync.Mutex
-	enterHook    func()
-	exitHook     func()
 }
 
 type condition func(from *State, to *State) bool
 
 type transition struct {
-	from       *State
-	to         *State
-	conditions []condition
+	from *State
+	to   *State
+	cond condition
 }
 
 var (
 	done = make(chan struct{})
 
-	commands = []string{
-		"cat <<EOF > /tmp/asdf",
-		`\
-#!/bin/bash
-hostname && echo OK
-`,
-		"EOF",
-		"chmod 755 /tmp/asdf;",
-		"/tmp/asdf;",
-	}
+	state_undefined        = State{name: "undefined"}
+	state_loggedoff        = State{name: "loggedoff"}
+	state_usernamesent     = State{name: "usernamesent"}
+	state_awaitingPassword = State{name: "awaitingPassword"}
+	state_passwordsent     = State{name: "passwordsent"}
+	state_loggedin         = State{name: "loggedin"}
+	state_loginfailed      = State{name: "loginfailed"}
+	state_error            = State{name: "error"}
 
-	start = State{
-		name: "undefined",
-		enterHook: func() {
-			fmt.Println("enterHook of 'undefined' called")
-		},
-		exitHook: func() {
-			fmt.Println("exitHook of 'undefined' called")
-		},
-	}
-	loggedoff = State{
-		name: "loggedoff",
-		enterHook: func() {
-			fmt.Println("enterHook of 'loggedoff' called")
-		},
-		exitHook: func() {
-			fmt.Println("exitHook of 'loggedoff' called")
-		},
-	}
-	loggedin = State{
-		name: "loggedin",
-		enterHook: func() {
-			fmt.Println("enterHook of 'loggedin' called")
-		},
-		exitHook: func() {
-			fmt.Println("exitHook of 'loggedin' called")
-		},
-	}
-	prompt = State{
-		name: "prompt",
-		enterHook: func() {
-			fmt.Println("enterHook of 'prompt' called")
-		},
-		exitHook: func() {
-			fmt.Println("exitHook of 'prompt' called")
-		},
-	}
-	busy = State{
-		name: "busy",
-		enterHook: func() {
-			fmt.Println("enterHook of 'busy' called")
-		},
-		exitHook: func() {
-			fmt.Println("exitHook of 'busy' called")
-		},
-	}
-	exit = State{
-		name: "exit",
-		enterHook: func() {
-			fmt.Println("enterHook of 'exit' called")
-			done <- struct{}{}
-		},
-		exitHook: func() {
-			fmt.Println("exitHook of 'exit' called")
-		},
-	}
-
-	currentstate *State = &start
+	currentstate *State = &state_undefined
 
 	transitions = []transition{
 		transition{
-			from: &start, to: &loggedoff,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					switch {
-					case strings.Index(string(from.received), " login: ") != -1:
-						return true
-					case strings.Index(string(from.received), "\nLogin incorrect ") != -1:
-						return true
-					case strings.Index(string(from.received), "\nPassword:") != -1:
-						from.received = []byte{}
-						cmd := "exit\n"
-						writerinput <- udsMessage{
-							typ:     udsmsg_host2serial,
-							len:     uint32(len(cmd)),
-							payload: []byte(cmd),
-						}
-						return true
-					}
-					return false
-				},
-			},
-		},
-		transition{
-			from: &loggedoff, to: &loggedoff,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					switch {
-					case strings.Index(string(from.received), "\nPassword:") != -1 &&
-						strings.Index(string(from.sent), username) == -1:
-						cmd := "\n"
-						writerinput <- udsMessage{
-							typ:     udsmsg_host2serial,
-							len:     uint32(len(cmd)),
-							payload: []byte(cmd),
-						}
-						return true
-					case strings.Index(string(from.received), "\nLogin timed out") != -1:
-						fallthrough
-					case strings.Index(string(from.received), "\nLogin incorrect") != -1:
-						return true
-					}
-
-					return false
-				},
-			},
-		},
-		transition{
-			from: &loggedoff, to: &loggedin,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					switch {
-					case strings.Index(string(from.received), "\nPassword:") != -1:
-						from.received = []byte{}
-						cmd := password + "\n"
-						writerinput <- udsMessage{
-							typ:     udsmsg_host2serial,
-							len:     uint32(len(cmd)),
-							payload: []byte(cmd),
-						}
-					case strings.Index(string(from.received), " login: ") != -1 &&
-						strings.Index(string(from.sent), password) == -1:
-						from.received = []byte{}
-						cmd := username + "\n"
-						writerinput <- udsMessage{
-							typ:     udsmsg_host2serial,
-							len:     uint32(len(cmd)),
-							payload: []byte(cmd),
-						}
-					case strings.Index(string(from.received), "\nLast login: ") != -1 &&
-						strings.Index(string(from.sent), password) != -1:
-						return true
-					}
-
-					return false
-				},
-			},
-		},
-		transition{
-			from: &loggedin, to: &prompt,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					userprompt := regexp.MustCompile(username + `@.*:~\$ `)
-					rootprompt := regexp.MustCompile(`root@.*:~\# `)
-					switch {
-					case rootprompt.Match(currentstate.received):
-						return true
-					case userprompt.Match(currentstate.received):
-						return true
-					default:
-						return false
-					}
-				},
-			},
-		},
-		transition{
-			from: &prompt, to: &exit,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					if len(commands) == 0 {
-						cmd := "exit\n"
-						writerinput <- udsMessage{
-							typ:     udsmsg_host2serial,
-							len:     uint32(len(cmd)),
-							payload: []byte(cmd),
-						}
-						return true
-					}
-					return false
-				},
-			},
-		},
-		transition{
-			from: &prompt, to: &busy,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					if len(commands) > 0 {
-						cmd := commands[0]
-						if cmd[len(cmd)-1] != '\n' {
-							cmd += "\n"
-						}
-						commands = commands[1:]
-						writerinput <- udsMessage{
-							typ:     udsmsg_host2serial,
-							len:     uint32(len(cmd)),
-							payload: []byte(cmd),
-						}
-						return true
-					}
-					return false
-				},
-			},
-		},
-		transition{
-			from: &busy, to: &prompt,
-			conditions: []condition{
-				func(from *State, to *State) bool {
-					userprompt := regexp.MustCompile(username + `@.*:~\$ `)
-					rootprompt := regexp.MustCompile(`root@.*:~\# `)
-					switch {
-					case rootprompt.Match(currentstate.received):
-						return true
-					case userprompt.Match(currentstate.received):
-						return true
-					default:
-						return false
-					}
-				},
-			},
+			from: &state_undefined, to: &state_loggedoff,
 		},
 	}
 )
 
 func statemachine() chan struct{} {
-	start.entranceTime = time.Now()
+	currentstate.entranceTime = time.Now()
 
 	go func() {
 	loop:
@@ -267,18 +53,8 @@ func statemachine() chan struct{} {
 			case <-time.After(time.Second):
 				valid, newState := checkTransitions()
 				if valid {
-					if currentstate.exitHook != nil {
-						currentstate.exitHook()
-					}
-					//sent := currentstate.sent
-					//received := currentstate.received
 					currentstate = newState
-					currentstate.sent = []byte{}
-					currentstate.received = []byte{}
 					currentstate.entranceTime = time.Now()
-					if currentstate.enterHook != nil {
-						currentstate.enterHook()
-					}
 					log.Println("Found new next state: ", currentstate.name)
 				}
 			}
